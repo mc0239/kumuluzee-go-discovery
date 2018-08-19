55c8587d2e6fc9bea073100f71d84d11753ff34c
Martin <martin.cebular@gmail.com>
2018-08-19 18:20:13 +0200

    removed logger, added *Options structs; added quite some to consul_discovery; updated README.md

--- discovery/consul_discovery.go
+++ discovery/consul_discovery.go
@@ -7,13 +7,28 @@ import (
 
 	"github.com/hashicorp/consul/api"
 	"github.com/mc0239/kumuluzee-go-config/config"
+	"github.com/mc0239/logm"
+	"github.com/satori/go.uuid"
 )
 
 type consulDiscoverySource struct {
-	client *api.Client
+	client               *api.Client
+	logger               *logm.Logm
+	registerableServices []registerableService
 }
 
-func initConsulDiscoverySource(config config.ConfigUtil) discoverySource {
+type registerableService struct {
+	isRegistered bool
+
+	id         string
+	port       int
+	name       string
+	versionTag string
+
+	options *RegisterOptions
+}
+
+func initConsulDiscoverySource(config config.Util, logger *logm.Logm) discoverySource {
 	clientConfig := api.DefaultConfig()
 
 	if addr, ok := config.GetString("kumuluzee.config.consul.hosts"); ok {
@@ -22,73 +37,122 @@ func initConsulDiscoverySource(config config.ConfigUtil) discoverySource {
 
 	client, err := api.NewClient(clientConfig)
 	if err != nil {
-		lgr.logE(fmt.Sprintf("Failed to create Consul client: %s", err.Error()))
+		logger.Error(fmt.Sprintf("Failed to create Consul client: %s", err.Error()))
 	}
 
-	lgr.logI(fmt.Sprintf("Consul client address set to %v", clientConfig.Address))
+	logger.Info(fmt.Sprintf("Consul client address set to %v", clientConfig.Address))
 
 	d := consulDiscoverySource{
 		client: client,
+		logger: logger,
 	}
 	return d
 }
 
-func (d consulDiscoverySource) RegisterService() (serviceID string, err error) {
-	envName, ok := conf.GetString("kumuluzee.env.name")
-	if !ok {
-		envName = "dev"
+func (d consulDiscoverySource) RegisterService(options RegisterOptions) (serviceID string, err error) {
+
+	regService := registerableService{
+		options: &options,
 	}
 
-	serName, ok := conf.GetString("kumuluzee.name")
-	if !ok {
-		serName = "unnamed"
+	// TODO: at some point, unregistered services should be removed from array!
+	d.registerableServices = append(d.registerableServices, regService)
+
+	// if * exists in config, override value in options
+	if name, ok := conf.GetString("kumuluzee.name"); ok {
+		regService.options.Value = name
 	}
 
-	version, ok := conf.GetString("kumuluzee.version")
-	if !ok {
-		version = "1.0.0"
+	if port, ok := conf.GetInt("kumuluzee.server.http.port"); ok {
+		regService.port = port
+	} else {
+		regService.port = 80 // TODO: default port to register?
 	}
 
-	/*var deregTime int
-	deregTime, ok = conf.GetInt("kumuluzee.config.consul.deregister-critical-service-after-s")
-	if !ok {
-		deregTime = 60
-	}*/
-
-	reg := api.AgentServiceRegistration{
-		//ID:   "kek2",
-		Name:    envName + "-" + serName,
-		Tags:    []string{"version=" + version},
-		Port:    9000,
-		Address: "192.168.2.50",
-		Checks:  api.AgentServiceChecks{
-			/*&api.AgentServiceCheck{
-				Name:     "health failure",
-				Interval: "10s",
-				HTTP:     "http://127.0.0.1:9000/health?s=fail",
-				DeregisterCriticalServiceAfter: strconv.Itoa(deregTime) + "s",
-			},*/
-			/*&api.AgentServiceCheck{
-				Name:     "health alright",
-				Interval: "30s",
-				HTTP:     "http://127.0.0.1:9000/health?s=ok",
-				DeregisterCriticalServiceAfter: strconv.Itoa(deregTime) + "s",
-			},*/
-			/*&api.AgentServiceCheck{
-				Name:     "health warning",
-				Interval: "10s",
-				HTTP:     "http://127.0.0.1:9000/health?s=warn",
-				DeregisterCriticalServiceAfter: "20s",
-			},*/
-		},
+	if env, ok := conf.GetString("kumuluzee.env.name"); ok {
+		regService.options.Environment = env
 	}
 
-	err = d.client.Agent().ServiceRegister(&reg)
+	if ver, ok := conf.GetString("kumuluzee.version"); ok {
+		regService.options.Version = ver
+	}
+
+	uuid4, err := uuid.NewV4()
 	if err != nil {
-		return "", err
+		d.logger.Error(fmt.Sprintf(err.Error()))
 	}
 
-	return reg.Name, nil // since id is not specified it equals the name of the service
+	regService.id = regService.options.Value + "-" + uuid4.String()
+	regService.name = regService.options.Environment + "-" + regService.options.Value
+	regService.versionTag = "version=" + regService.options.Version
+
+	if isRegistered := d.isServiceRegistered(regService); isRegistered && regService.options.Singleton {
+		d.logger.Info("Service already registered, not registering with songleton option true ...")
+	} else {
+		d.logger.Info("Registering service ...")
+
+		regErr := d.client.Agent().ServiceRegister(&api.AgentServiceRegistration{
+			Port: regService.port,
+			ID:   regService.id,
+			Name: regService.name,
+			Tags: []string{"<service protocol>", regService.versionTag},
+			Check: &api.AgentServiceCheck{
+				TTL: strconv.FormatInt(regService.options.TTL, 10) + "s",
+				DeregisterCriticalServiceAfter: strconv.FormatInt(10, 10) + "s",
+			},
+		})
+
+		if regErr != nil {
+			d.logger.Error("Service register fail ...")
+			// retry delay stuff somehow
+		}
+
+		regService.isRegistered = true
+	}
+
+	return regService.id, nil // since id is not specified it equals the name of the service
+}
+
+func (d consulDiscoverySource) isServiceRegistered(reg registerableService) bool {
+	serviceEntries, _, err := d.client.Health().Service(reg.id, reg.versionTag, true, nil)
+
+	if err != nil {
+		d.logger.Error(fmt.Sprintf("%s", err.Error()))
+		return false
+	}
+
+	for _, service := range serviceEntries {
+		for _, tag := range service.Service.Tags {
+			if tag == reg.versionTag {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (d consulDiscoverySource) sendHeartbeat(reg registerableService) {
+	d.logger.Verbose("Sending heartbeat....")
+
+	checks, err := d.client.Agent().Checks()
+	if err != nil {
+		d.logger.Error("asdasdasdsd.......")
+	}
+
+	var serviceOk bool
+	for _, check := range checks {
+		if check.ServiceID == reg.id && check.Status == "passing" {
+			serviceOk = true
+		}
+	}
+
+	if !serviceOk {
+		reg.isRegistered = false
+		d.logger.Error("SERVCE not registered when tried hearbeating ...")
+		d.RegisterService(*reg.options)
+	}
+
 }
 
 func (d consulDiscoverySource) DiscoverService(name string, tag string, passing bool) (Service, error) {
--- discovery/discovery.go
+++ discovery/discovery.go
@@ -1,19 +1,72 @@
 package discovery
 
 import (
-	"errors"
-
 	"github.com/mc0239/kumuluzee-go-config/config"
+	"github.com/mc0239/logm"
 )
 
-var conf config.ConfigUtil
+var conf config.Util
 
-type DiscoveryUtil struct {
-	discoverySources []discoverySource
-	Logger           logger
+// Options struct is used when instantiating a new Util.
+type Options struct {
+	// Additional configuration source to connect to. Possible values are: "consul"
+	Extension string
+	// FilePath is a path to configuration file, including the configuration file name.
+	// Passing an empty string will default to config/config.yaml
+	FilePath string
+	// LogLevel can be used to limit the amount of logging output. Default log level is 0. Level 4
+	// will only output Warnings and Errors, and level 5 will only output errors.
+	// See package github.com/mc0239/logm for more details on logging and log levels.
+	LogLevel int
 }
 
-var lgr *logger
+// RegisterOptions is used when registering a service
+type RegisterOptions struct {
+	// Service name to register the service by.
+	// Can be overridden with configuration key kumuluzee.name
+	Value string
+	// Time to live of a registration key in the store (in seconds).
+	// Default value is 30.
+	// Can be overridden with configuration key kumuluzee.discovery.ttl
+	TTL int64
+	// Interval in which service updates registration key value in store.
+	// Default value is 20.
+	// Can be overridden with configuration key kumuluzee.discovery.ping-interval
+	PingInterval int64
+	// Environment in which the service is registered.
+	// Default value is "dev".
+	// Can be overridden with configuration key kumuluzee.env.name
+	Environment string
+	// Version of the service to be registered.
+	// Default value is "1.0.0".
+	// Can be overridden with configuration key kumuluzee.version
+	Version string
+	// If set to true, only once instance of service with the same name, version and environment is registered.
+	// Default value is false.
+	Singleton bool
+}
+
+type DiscoverOptions struct {
+	// Name of the service to discover.
+	Value string
+	// Environment of the service to discover.
+	// If value is not provided, it uses value from configuration with key kumuluzee.env.name
+	// If value is not specified and key in configuration does not exists, value defaults to 'dev'.
+	Environment string
+	// Version of the service to discover.
+	// Uses semantic versioning?
+	// Default value is "*", which resolves to highest deployed version.
+	Version string
+	// TODO
+	AccessType string
+}
+
+// Util is used for registering and discovering services from a service discovery source.
+// Util should be initialized with discovery.New() function
+type Util struct {
+	discoverySource discoverySource
+	Logger          logm.Logm
+}
 
 type Service struct {
 	Address string
@@ -21,50 +74,41 @@ type Service struct {
 }
 
 type discoverySource interface {
-	RegisterService() (serviceID string, err error)
+	RegisterService(options RegisterOptions) (serviceID string, err error)
 	DiscoverService(name string, tag string, passing bool) (Service, error)
 }
 
-func Initialize(ext string, configPath string, logLevel int) DiscoveryUtil {
+func New(options Options) Util {
 
-	lgr = &logger{
-		LogLevel: logLevel,
-	}
+	lgr := logm.New("Kumuluz-discovery")
 
-	conf = config.Initialize("", "", 2)
+	conf = config.NewUtil(config.Options{
+		FilePath: options.FilePath,
+		LogLevel: logm.LvlWarning,
+	})
 
 	var src discoverySource
 
-	if ext == "consul" {
-		src = initConsulDiscoverySource(conf) // TODO should actually pass file source
-	} else if ext == "etcd" {
+	if options.Extension == "consul" {
+		src = initConsulDiscoverySource(conf, &lgr) // TODO should actually pass file source
+	} else if options.Extension == "etcd" {
 		// TODO:
 	} else {
 		// TODO: invalid ext
 	}
 
-	k := DiscoveryUtil{
-		[]discoverySource{src},
-		*lgr,
+	k := Util{
+		src,
+		lgr,
 	}
 
 	return k
 }
 
-func (d DiscoveryUtil) RegisterService() (string, error) {
-
-	for _, ds := range d.discoverySources {
-		return ds.RegisterService()
-	}
-
-	return "", errors.New("Service registration failed with all clients")
+func (d Util) RegisterService(options RegisterOptions) (string, error) {
+	return d.discoverySource.RegisterService(options)
 }
 
-func (d DiscoveryUtil) DiscoverService(name string, tag string, passing bool) (Service, error) {
-
-	for _, ds := range d.discoverySources {
-		return ds.DiscoverService(name, tag, passing)
-	}
-
-	return Service{}, errors.New("Service discovery failed with all clients")
+func (d Util) DiscoverService(name string, tag string, passing bool) (Service, error) {
+	return d.discoverySource.DiscoverService(name, tag, passing)
 }
