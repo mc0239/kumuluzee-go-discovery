5a8e638f95e38ab7a4637cca3f645bcaabbd7b96
Martin <martin.cebular@gmail.com>
2018-08-25 16:46:06 +0200

    Added checkin (TTL) and register; version comparison for discovery

--- discovery/consul_discovery.go
+++ discovery/consul_discovery.go
@@ -3,8 +3,10 @@ package discovery
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/blang/semver"
 	"github.com/hashicorp/consul/api"
 	"github.com/mc0239/kumuluzee-go-config/config"
 	"github.com/mc0239/logm"
@@ -12,7 +14,11 @@ import (
 )
 
 type consulDiscoverySource struct {
-	client               *api.Client
+	client *api.Client
+
+	startRetryDelay int64
+	maxRetryDelay   int64
+
 	logger               *logm.Logm
 	registerableServices []registerableService
 }
@@ -31,11 +37,29 @@ type registerableService struct {
 }
 
 func initConsulDiscoverySource(config config.Util, logger *logm.Logm) discoverySource {
+	logger.Verbose("Initializing ConsulDiscoverySource")
+	var d consulDiscoverySource
+	d.logger = logger
+
 	clientConfig := api.DefaultConfig()
 
 	if addr, ok := config.GetString("kumuluzee.config.consul.hosts"); ok {
 		clientConfig.Address = addr
 	}
+
+	startRetryDelay, ok := config.Get("kumuluzee.config.start-retry-delay-ms").(float64)
+	if !ok {
+		logger.Error("Failed to assert value kumuluzee.config.start-retry-delay-ms as float64. Using default value 500.")
+		startRetryDelay = 500
+	}
+	d.startRetryDelay = int64(startRetryDelay)
+
+	maxRetryDelay, ok := config.Get("kumuluzee.config.max-retry-delay-ms").(float64)
+	if !ok {
+		logger.Error("Failed to assert value kumuluzee.config.max-retry-delay-ms as float64. Using default value 900000.")
+		maxRetryDelay = 900000
+	}
+	d.maxRetryDelay = int64(maxRetryDelay)
 
 	client, err := api.NewClient(clientConfig)
 	if err != nil {
@@ -44,10 +68,8 @@ func initConsulDiscoverySource(config config.Util, logger *logm.Logm) discoveryS
 
 	logger.Info("Consul client address set to %s", clientConfig.Address)
 
-	d := consulDiscoverySource{
-		client: client,
-		logger: logger,
-	}
+	d.client = client
+
 	return d
 }
 
@@ -90,6 +112,13 @@ func (d consulDiscoverySource) RegisterService(options RegisterOptions) (service
 		regService.options.TTL = 30
 	}
 
+	if pingInterval, ok := conf.GetInt("kumuluzee.discovery.ping-interval"); ok {
+		regService.options.PingInterval = int64(pingInterval)
+	}
+	if regService.options.PingInterval == 0 {
+		regService.options.PingInterval = 20
+	}
+
 	uuid4, err := uuid.NewV4()
 	if err != nil {
 		d.logger.Error(fmt.Sprintf(err.Error()))
@@ -99,39 +128,9 @@ func (d consulDiscoverySource) RegisterService(options RegisterOptions) (service
 	regService.name = regService.options.Environment + "-" + regService.options.Value
 	regService.versionTag = "version=" + regService.options.Version
 
-	if isRegistered := d.isServiceRegistered(&regService); isRegistered && regService.options.Singleton {
-		d.logger.Error("Service is already registered, not registering with options.singleton set to true")
-	} else {
-		d.logger.Info("Registering service: id=%s address=%s port=%d", regService.id, regService.address, regService.port)
+	d.register(&regService, d.startRetryDelay)
 
-		agentRegistration := api.AgentServiceRegistration{
-			Port: regService.port,
-			ID:   regService.id,
-			Name: regService.name,
-			Tags: []string{"<service protocol>", regService.versionTag},
-			Check: &api.AgentServiceCheck{
-				CheckID: "check-" + regService.id,
-				TTL:     strconv.FormatInt(regService.options.TTL, 10) + "s",
-				DeregisterCriticalServiceAfter: strconv.FormatInt(10, 10) + "s",
-			},
-		}
-
-		if regService.address != "" {
-			agentRegistration.Address = regService.address
-		}
-
-		err = d.client.Agent().ServiceRegister(&agentRegistration)
-
-		if err != nil {
-			d.logger.Error(fmt.Sprintf("Service registration failed: %s", err.Error()))
-			// retry delay stuff somehow
-		}
-
-		d.logger.Info("Service registered, id=%s", regService.id)
-		regService.isRegistered = true
-	}
-
-	go d.checkIn(&regService)
+	go d.checkIn(&regService, d.startRetryDelay)
 
 	return regService.id, nil
 }
@@ -155,22 +154,78 @@ func (d consulDiscoverySource) isServiceRegistered(reg *registerableService) boo
 	return false
 }
 
-func (d consulDiscoverySource) checkIn(reg *registerableService) {
+func (d consulDiscoverySource) register(reg *registerableService, retryDelay int64) {
+	if isRegistered := d.isServiceRegistered(reg); isRegistered && reg.options.Singleton {
+		d.logger.Error("Service is already registered, not registering with options.singleton set to true")
+	} else {
+		d.logger.Info("Registering service: id=%s address=%s port=%d", reg.id, reg.address, reg.port)
+
+		agentRegistration := api.AgentServiceRegistration{
+			Port: reg.port,
+			ID:   reg.id,
+			Name: reg.name,
+			Tags: []string{"<service protocol>", reg.versionTag},
+			Check: &api.AgentServiceCheck{
+				CheckID: "check-" + reg.id,
+				TTL:     strconv.FormatInt(reg.options.TTL, 10) + "s",
+				DeregisterCriticalServiceAfter: strconv.FormatInt(10, 10) + "s",
+			},
+		}
+
+		if reg.address != "" {
+			agentRegistration.Address = reg.address
+		}
+
+		err := d.client.Agent().ServiceRegister(&agentRegistration)
+
+		if err != nil {
+			d.logger.Error(fmt.Sprintf("Service registration failed: %s", err.Error()))
+			// sleep for current delay
+			time.Sleep(time.Duration(retryDelay) * time.Millisecond)
+
+			// exponentially extend retry delay, but keep it at most maxRetryDelay
+			newRetryDelay := retryDelay * 2
+			if newRetryDelay > d.maxRetryDelay {
+				newRetryDelay = d.maxRetryDelay
+			}
+			d.register(reg, newRetryDelay)
+			return
+		}
+
+		d.logger.Info("Service registered, id=%s", reg.id)
+		reg.isRegistered = true
+	}
+}
+
+func (d consulDiscoverySource) checkIn(reg *registerableService, retryDelay int64) {
 	d.logger.Verbose("Updating TTL for service %s", reg.id)
 
 	err := d.client.Agent().UpdateTTL("check-"+reg.id, "serviceid="+reg.id+" time="+time.Now().Format("2006-01-02 15:04:05"), "passing")
 	if err != nil {
-		d.logger.Error("Updating TTL failed for service %s, error: %s", reg.id, err.Error())
+		d.logger.Error("Updating TTL failed for service %s, error: %s, retry delay: %d ms", reg.id, err.Error(), retryDelay)
+
+		// sleep for current delay
+		time.Sleep(time.Duration(retryDelay) * time.Millisecond)
+
+		// exponentially extend retry delay, but keep it at most maxRetryDelay
+		newRetryDelay := retryDelay * 2
+		if newRetryDelay > d.maxRetryDelay {
+			newRetryDelay = d.maxRetryDelay
+		}
+		d.checkIn(reg, newRetryDelay)
+		return
 	}
 
 	time.Sleep(time.Duration(reg.options.PingInterval) * time.Second)
-	d.checkIn(reg)
+	d.checkIn(reg, d.startRetryDelay)
+	return
 }
 
 func (d consulDiscoverySource) DiscoverService(options DiscoverOptions) (Service, error) {
 
-	// TODO: ENV? ACCESSTYPE?
-	serviceEntries, _, err := d.client.Health().Service(options.Value, "version="+options.Version, true, nil)
+	// TODO: ACCESSTYPE?
+	queryServiceName := options.Environment + "-" + options.Value
+	serviceEntries, _, err := d.client.Health().Service(queryServiceName, "", true, nil)
 	if err != nil {
 		d.logger.Error("Service discovery failed: %s", err.Error())
 		return Service{}, fmt.Errorf("Service discovery failed: %s", err.Error())
@@ -179,19 +234,45 @@ func (d consulDiscoverySource) DiscoverService(options DiscoverOptions) (Service
 	var addr string
 	var port int
 
+	d.logger.Verbose("Services %s-%s available: %d", options.Environment, options.Value, len(serviceEntries))
+
 	for _, serviceEntry := range serviceEntries {
-		addr = serviceEntry.Service.Address
-		if addr == "" {
-			addr = serviceEntry.Node.Address
+		wantVersion, err := semver.Parse(options.Version)
+		if err != nil {
+			return Service{}, err
 		}
-		port = serviceEntry.Service.Port
 
-		d.logger.Verbose("Found service, address=%s port=%d", addr, port)
+		var foundService = false
+		for _, tag := range serviceEntry.Service.Tags {
+			if strings.HasPrefix(tag, "version") {
+				t := strings.Split(tag, "=")
+				gotVersion, err := semver.Parse(t[1])
+				if err == nil {
+					if gotVersion.Compare(wantVersion) == 0 {
+						foundService = true
+						break
+					}
+				} else {
+					d.logger.Warning("Semantic version parsing failed for: %s, error: %s", t[1], err.Error())
+				}
+				break
+			}
+		}
 
-		return Service{
-			Address: addr,
-			Port:    port,
-		}, nil
+		if foundService {
+			addr = serviceEntry.Service.Address
+			if addr == "" {
+				addr = serviceEntry.Node.Address
+			}
+			port = serviceEntry.Service.Port
+
+			d.logger.Verbose("Found service, address=%s port=%d", addr, port)
+
+			return Service{
+				Address: addr,
+				Port:    port,
+			}, nil
+		}
 	}
 
 	return Service{}, fmt.Errorf("Service discovery failed: No services for given query") // TODO: check if really
